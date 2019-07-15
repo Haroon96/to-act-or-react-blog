ec20d8f1d74ba0edc9cd7e87cd6316c1146a7084
Muhammad Haroon <m_haroon96@hotmail.com>
2019-07-15 23:37:12 +0500

    added dynamic content + restructured

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function scrollTo(id) {
     console.log(id);
     document.getElementById(id).scrollIntoView();
 }
+
+function removeLoader(id) {
+    let elem = document.getElementById(id);
+    elem.parentNode.removeChild(elem);
+}
